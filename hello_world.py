4b78b22cd0d0e4ddc64173a16ebed66dc5cec1b6
Yugesh Madhavan <yugesh@seriesfi.com>
2024-03-07 15:44:17 -0600

    Update: Added cleanup function to remove unnecessary characters from AI response.

--- hello_world.py
+++ hello_world.py
@@ -5,17 +5,35 @@ llm = Ollama(model="codellama", temperature=0.6)
 def get_response(prompt):
     return llm.invoke(prompt)
 
+def clean_commit_message(response):
+    cleanedCommitMessage = []
+    listOfLines =  response.splitlines()
+    for line in listOfLines:
+        if("COMMIT MESSAGE" not in line.upper()):
+            cleanedCommitMessage.append(f"{line}\n")
+    return ''.join(str(x) for x in cleanedCommitMessage).strip()
+
 def get_staged_changeset():
     return subprocess.run(["git", "diff", "--cached"], capture_output=True).stdout.splitlines()
 
-def main():
-    currentDiff = get_staged_changeset()
+def is_staged_changes(currentDiff):
     if(len(currentDiff) == 0):
         print("No staged changes")
+        return False
+    return True
+
+def main():
+    currentDiff = get_staged_changeset()
+    if(not is_staged_changes(currentDiff)):
         return
     response = get_response(f"You are an expert programmer that writes simple, concise code and explanations. Write a commit message for the following diff (I am using this in a program so give me JUST the message): {currentDiff}")
-    # response = get_response(f"Assume you are a developer and you are writing a succinct but clear commit message for the following changeset: {get_staged_changeset()}")
-    print(response)
+    cleanedCommitMessage = clean_commit_message(response)
+    if(cleanedCommitMessage == "" or cleanedCommitMessage == "\n"):
+        print("No commit message available after cleanup - returning original response from AI")
+        print(f"Original response: {response}")
+        return response
+    print(f"Commit message: {cleanedCommitMessage}")
+    return cleanedCommitMessage
 
 if __name__ == "__main__":
     main()
