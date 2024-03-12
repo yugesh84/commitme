081b3dcb81c9a4282cca05271191a637c5610a9c
Yugesh Madhavan <yugesh@seriesfi.com>
2024-03-12 13:48:04 -0500

    1. Added a configuration file `commitme.properties.json` to store preferred properties including the choice of AI model (local or OpenAI) and relevant details for each. 2. Updated `.gitignore` to ignore the properties file. 4. Added dependencies in `requirements.txt` for supporting OpenAI. 5. Introduced two new functions `get_response_from_open_ai` and `get_response_from_local_ai` to fetch AI generated responses based on the chosen model.

--- commitme.py
+++ commitme.py
@@ -1,8 +1,10 @@
+import json
 from langchain_community.llms import Ollama
 import subprocess
-llm = Ollama(model="codellama", temperature=0.6)
 
-def get_response(prompt):
+def get_response_from_local_ai(prompt, properties):
+    localRunningModelName = properties.get("localModel",{}).get("name","codellama")
+    llm = Ollama(model=localRunningModelName, temperature=0.6)
     return llm.invoke(prompt)
 
 def clean_commit_message(response):
@@ -29,28 +31,56 @@ def calculate_final_commit_message(response):
     cleanedCommitMessage = clean_commit_message(response)
     if(cleanedCommitMessage == "" or cleanedCommitMessage == "\n"):
         print("No commit message available after cleanup - returning original response from AI")
-        print(f"Original response: {response}")
         return response
-    print(f"Commit message: {cleanedCommitMessage}")
     return cleanedCommitMessage
 
-def dowork(currentDiff):
-    response = get_response(f"You are an expert programmer that writes simple, concise code and explanations. Write a commit message for the following diff (I am using this in a program so give me JUST the message): {currentDiff}")
+def get_response_from_ai(currentDiff, properties):
+    if(properties.get("use") == "openAI"):
+        response = get_response_from_open_ai(currentDiff, properties)
+    else:
+        response = get_response_from_local_ai(f"You are an expert programmer that writes simple, concise code and explanations. Write a commit message for the following diff (I am using this in a program so give me JUST the message): {currentDiff}", properties)
     return calculate_final_commit_message(response)
 
-def commit_when_happy(currentDiff):
+def commit_when_happy(currentDiff, properties):
     while True:
-        finalCommitMessage = dowork(currentDiff)
+        finalCommitMessage = get_response_from_ai(currentDiff, properties)
         print(f"Final commit message: {finalCommitMessage}")
         if(input("Are you happy with this commit message? (y/n) ").lower() == "y"):
             break
     commit(finalCommitMessage)
 
+def get_response_from_open_ai(currentDiff, properties):
+    from openai import OpenAI
+
+    client = OpenAI(
+        organization=properties.get("openAI",{}).get("organizationId",""),
+        api_key=properties.get("openAI",{}).get("apiKey","")
+    )
+    from openai import OpenAI
+
+    collectedData = ""
+    stream = client.chat.completions.create(
+        model=properties.get("openAI",{}).get("model",properties.get("openAI",{}).get("model","gpt-4")),
+        messages=[{"role": "user", "content": f"Can you write a git commit message for this diff? {currentDiff}"}],
+        stream=True,
+    )
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            collectedData += chunk.choices[0].delta.content
+    return collectedData
+
+def load_properties():
+    with open("commitme.properties.json") as f:
+        properties = json.load(f)
+    return properties
+
 def main():
+    properties = load_properties()
     currentDiff = get_staged_changeset()
     if(not is_staged_changes(currentDiff)):
         return
-    commit_when_happy(currentDiff)
+    commit_when_happy(currentDiff, properties)
+
 
 if __name__ == "__main__":
     main()
